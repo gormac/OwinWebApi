9027c9a32bccd252ee84b75a241e8a31e90f3b1d
Ronald van Leuwen <rvleuwen@gmail.com>
2016-12-21 20:48:23 +0100

    Add project files.

---
+++ Rovale.OwinWebApi/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rovale.OwinWebApi
+{
+    public class Class1
+    {
+    }
+}
